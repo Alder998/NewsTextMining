71abcc4b3c02b7d184a2f950f30f5e601fc14f72
Alder998 <davide.ontano@yahoo.com>
2023-07-06 18:27:16 +0200

    Single stock News Update

---
+++ Test.py
@@ -0,0 +1,14 @@
+import NewsDownloadLibrary as nl
+
+print('\n')
+print('-----NEWS PANO-----')
+print('\n')
+
+pano = nl.getNewsWithIndexPerformance()
+
+print('\n')
+print('-----NEWS DETAILS-----')
+print('\n')
+
+massive = nl.MassiveNewsScaper()
+
