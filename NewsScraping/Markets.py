88edf36c960ec213396b8a6bee8a99b36d881f3c
Alder998 <davide.ontano@yahoo.com>
2023-10-01 22:25:36 +0200

    Fast Scraping to update index Components on postgres Database

--- NewsScraping/Markets.py
+++ NewsScraping/Markets.py
@@ -44,7 +44,7 @@ class Markets:
                 if lastTrade.date() == datetime.today().date():
                     # Check the last trade Hour to be the same hour of now (just with hours,because with minutes as
                     # well could be misleading
-                    if lastTrade.hour == datetime.today.hour:
+                    if lastTrade.hour == datetime.today().hour:
                         openMarkets.append(stocks['Country'][stocks['YFTicker'] == index].reset_index()['Country'][0])
 
             else:
@@ -61,11 +61,140 @@ class Markets:
                 lastTrade = pd.to_datetime(index1.reset_index()['Date'].tail(1).reset_index()['Date'])[0]
 
                 if lastTrade.date() == datetime.today().date():
-                    if lastTrade.hour == datetime.today.hour:
+                    if lastTrade.hour == datetime.today().hour:
                         openMarketsFutures.append(futures['Country'][futures['YFTicker'] == index].reset_index()['Country'][0])
             else:
                 print('Not Found: check connection')
 
         return openMarkets + openMarketsFutures
 
-    #TODO: Implement the Stock index Picker according to open markets.
+
+    def getStockIndex (self):
+
+        import pandas as pd
+        import yfinance as yf
+        import psycopg2
+        from sqlalchemy import create_engine
+        from datetime import datetime
+
+        # Import the Database with all the stocks traded
+
+        engine = create_engine('postgresql://postgres:Davidescemo@localhost:5432/YahooFinance')
+        query = 'SELECT * FROM public."AllStockTraded"'
+        allStocks = pd.read_sql(query, engine)
+
+        # Get all the Open Markets
+
+        openMarkets = self.getOpenMarkets()
+
+        # Filter the allStocks sample for the open Markets
+
+        for market in openMarkets:
+            marketStocks = allStocks[allStocks['Country'] == market]
+
+
+        return 0
+
+    def updateIndexComponentsOnDatabase (self):
+
+        # Fast Scraping to update the index Components data
+
+        import requests
+        from bs4 import BeautifulSoup
+        import numpy as np
+        import pandas as pd
+        import psycopg2
+        from sqlalchemy import create_engine
+
+        engine = create_engine('postgresql://postgres:Davidescemo@localhost:5432/YahooFinance')
+        query = 'SELECT * FROM public."WorldIndexTickers"'
+        marketIndex = pd.read_sql(query, engine)
+
+        stockIndex = marketIndex[marketIndex['Type'] == 'StockIndex']
+
+        indexPresent = list()
+        for indexName in stockIndex['YFTicker']:
+
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
+
+            index = indexName.replace('^', '%5E')
+            target_url = "https://finance.yahoo.com/quote/" + index + "/components"
+
+            resp = requests.get(target_url, headers=headers)
+            soup = BeautifulSoup(resp.text, 'html.parser')  # Pare che l'URL sia libero per fare scraping
+
+            a_tag = soup.findAll('a')
+
+            fs = list()
+            for i in a_tag:
+                fs.append(i)
+
+            a = pd.Series(fs).astype(str)
+
+            rawData = a[(a.str.contains('href="/quote/')) & (~a.str.contains('aria'))].reset_index()
+            del [rawData['index']]
+
+            if rawData.empty == False:
+
+                componentsList = list()
+                for i in range(len(rawData[0])):
+                    start = rawData[0][i].find('href="/quote/') + len('href="/quote/')
+                    end = rawData[0][i][start:].find('?')
+                    comp = rawData[0][i][start:start + end]
+                    componentsList.append(comp)
+
+                componentForm = pd.Series(componentsList)
+                componentForm = pd.concat([componentForm, pd.Series(np.full(len(componentForm), indexName))],
+                                          axis=1).set_axis(['Component', 'Index'], axis=1)
+                indexPresent.append(componentForm)
+
+        indexPresent = pd.concat([series for series in indexPresent], axis=0).reset_index()
+        del [indexPresent['index']]
+
+        # Add SP500
+
+        SP = pd.read_excel(r"C:\Users\39328\OneDrive\Desktop\S&P500 Constituents.xlsx")
+
+        indexPresent = pd.concat([indexPresent, SP['Tciker']], axis=0).reset_index()
+        del [indexPresent['index']]
+
+        print(indexPresent)
+
+        # Import AllStocks DB
+
+        engine = create_engine('postgresql://postgres:Davidescemo@localhost:5432/YahooFinance')
+        query = 'SELECT * FROM public."AllStockTraded"'
+        allStocks = pd.read_sql(query, engine)
+
+        # indexPresent['Component'] = indexPresent['Component'].str.split('.').str[0]
+
+        print(indexPresent)
+
+        for stock in indexPresent['Component']:
+            allStocks.loc[allStocks['Ticker'] == stock, 'IndexPresent'] = 'Y'
+
+        # Save and Update
+
+        file = allStocks
+
+        connection = psycopg2.connect(
+            database="YahooFinance",
+            user="postgres",
+            password="Davidescemo",
+            host="localhost",
+            port="5432"
+        )
+
+        engine = create_engine('postgresql://postgres:Davidescemo@localhost:5432/YahooFinance')
+        # file.to_sql('AllStockTraded', engine, if_exists='replace', index=False)
+
+        print(allStocks)
+
+        print(allStocks.dropna())
+        print(allStocks[['Country', 'IndexPresent']].groupby('Country').count())
+
+
+
+
+
