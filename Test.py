f17f8d041843eabc63f86746dafd21ccfde926a7
Alder998 <davide.ontano@yahoo.com>
2023-10-27 20:19:14 +0200

    Setting changes (Disk is full)

--- Test.py
+++ Test.py
@@ -13,15 +13,15 @@ textList = pd.read_sql(query, engine)
 #textList = textList[0:500]
 
 # Preprocessing the data: taking english news, removing stop words, taking the words' root
-cleanData = ns.PreProcessing(textList, target='returns', classes=4, threshold=1.5, databaseVersion='V2').preProcess(POS_tagging=False)
+cleanData = ns.PreProcessing(textList, target='returns', classes=4, threshold=1.2, databaseVersion='V2').preProcess(POS_tagging=False)
 
 # Data Vectorization: turning text data into a vector, numerically processable by an algorithm
-BoWEmbedding = ns.Vectorize(cleanData).Embedding(method = 'Word2Vec', vectorSize=30, components = 200)
+BoWEmbedding = ns.Vectorize(cleanData).Embedding(method = 'Bag-of-Word', vectorSize=20, components = 200)
 
 # Setting the model, compile, train, evaluate the performance on a test set
 sample = ns.Sampling(BoWEmbedding, testSize=0.20).TrainTestSplit()
 
-#modelSet = ns.NNModel(sample, epochs=10).NNProcessing(NNType='FF', shapeRec=[64], shape = [200, 200, 200],
-#                                                                        activation = 'relu')
+modelSet = ns.NNModel(sample, epochs=10).NNProcessing(NNType='FF', shapeRec=[64], shape = [200, 200],
+                                                                        activation = 'relu')
 
-modelSet = ns.MLModel(sample).SVCProcessing(kernel = 'rbf')
+#modelSet = ns.MLModel(sample).SVCProcessing(kernel = 'rbf')
